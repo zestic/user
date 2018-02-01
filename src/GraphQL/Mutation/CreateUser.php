622532a753267c49e9539db72cd705e4e0d9076e
Shank <develop@zestic.com>
2018-02-01 09:13:20 -0800

    Create CreateUser.php

---
+++ src/GraphQL/Mutation/CreateUser.php
@@ -0,0 +1,13 @@
+<?php
+declare (strict_types=1);
+
+namespace Zestic\User\GraphQL\Mutation;
+
+use Pac\ProophPackage\GraphQL\ProophBridgeField;
+
+class CreateUserMutation extends ProophBridgeField
+{
+    public function build(FieldConfig $config)
+    {
+    }
+}
