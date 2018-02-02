343f1a3efee0487b5617022796dab11efd632c77
shank <develop@zestic.com>
2018-02-02 00:34:58 -0800

    bring zend expressive authentication in for middleware

---
+++ src/Entity/User.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Zestic\User\Entity;
+
+use Zend\Expressive\Authentication\UserInterface;
+
+final class User implements UserInterface
+{
+    private $emailAddress;
+    /** @var string */
+    private $identity;
+    /** @var string[] */
+    private $userRoles = [];
+
+    public function getIdentity(): string
+    {
+        return $this->identity;
+    }
+
+    public function getUserRoles(): array
+    {
+        return $this->userRoles;
+    }
+}
